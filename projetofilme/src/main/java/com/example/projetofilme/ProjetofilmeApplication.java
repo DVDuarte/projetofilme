cdda36a6d2affe93670fba66e193a10f42617dd5
IGOR FARIA NEVES <igor.neves3@granbery.edu.dti>
2024-04-30 22:15:57 -0300

    update

---
+++ projetofilme/src/main/java/com/example/projetofilme/ProjetofilmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetofilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofilmeApplication.class, args);
+	}
+
+}
